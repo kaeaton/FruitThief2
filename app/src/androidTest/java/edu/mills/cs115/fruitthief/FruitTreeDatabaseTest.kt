6156113670a3dc64dd540c793d21f32aadfe5e8b
hannela <hjane@mills.edu>
2021-04-24 11:13:11 -0700

    Add database files

---
+++ app/src/androidTest/java/edu/mills/cs115/fruitthief/FruitTreeDatabaseTest.kt
@@ -0,0 +1,46 @@
+package edu.mills.cs115.fruitthief
+
+import androidx.room.Room
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.platform.app.InstrumentationRegistry
+import edu.mills.cs115.fruitthief.database.FruitTreeDAO
+import edu.mills.cs115.fruitthief.database.FruitTreeDatabase
+import edu.mills.cs115.fruitthief.database.PopulateFruitTable
+import junit.framework.Assert.*
+import org.junit.Before
+import org.junit.After
+import org.junit.Test
+import org.junit.runner.RunWith
+import java.io.IOException
+
+@RunWith(AndroidJUnit4::class)
+class FruitTreeDatabaseTest {
+    private lateinit var fruitTreeDao: FruitTreeDAO
+    private lateinit var db: FruitTreeDatabase
+
+    @Before
+    fun createDb() {
+        val context = InstrumentationRegistry.getInstrumentation().targetContext
+        // Using an in-memory database because the information stored here disappears when the
+        // process is killed.
+        db = Room.inMemoryDatabaseBuilder(context, FruitTreeDatabase::class.java)
+            // Allowing main thread queries, just for testing.
+            .allowMainThreadQueries()
+            .build()
+        fruitTreeDao = db.fruitTreeDAO
+    }
+    @After
+    @Throws(IOException::class)
+    fun closeDb() {
+        db.close()
+    }
+
+    @Test
+    @Throws(Exception::class)
+    fun insertFruit(){
+        PopulateFruitTable(fruitTreeDao);
+        assertEquals("Banana", fruitTreeDao.getFruitByName("Banana").fruitName)
+        PopulateFruitTable(fruitTreeDao)
+        assertEquals("IJKL", fruitTreeDao.getFruitByName("Apple").fruitSeason)
+    }
+}
